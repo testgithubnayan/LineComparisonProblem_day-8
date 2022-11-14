296b8bfa829ada9a9b3e88e6f09e39afbdbd8912
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-14 17:41:07 +0530

    lenth

--- LineComparison.java
+++ LineComparison.java
@@ -1,16 +1,23 @@
 package com.bridgelabz;
 
-public class Line {
+import java.util.Scanner;
 
-    Point p1;
-    Point p2;
+public class LineComparison {
 
-    public Line(Point p1, Point p2) {
-        this.p1 = p1;
-        this.p2 = p2;
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computation program");
+        calculateLength();
     }
 
-    float getLength(){
-        return (float) Math.sqrt( Math.pow((this.p2.x- p1.x),2) + Math.pow((p2.y- p1.y),2) );
+    public static void calculateLength() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter coordinates of first point x1 y1 : ");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("Enter coordinates of second point x2 y2 : ");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        double length = Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2)));
+        System.out.println("length of first line is: " + length);
     }
 }
